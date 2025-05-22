cdf3ec888758ea4d82125aee8275e246dda202aa
andy <ramhenintsoa@gmail.com>
2025-05-22 12:12:30 +0300

    delete comp

--- next.config.mjs
+++ next.config.mjs
@@ -6,3 +6,4 @@ const nextConfig = {
 export default nextConfig;
 // Set-ExecutionPolicy -scope Process -ExecutionPolicy Bypass
 //ngrok http 3000d-authtoken 2xMwNPxoydpy9YNELxox38uKLZZ_2bRRW5gvSWRxKEzdHNbm7
+//ssh-keygen -t ed25519 -C "jayahnambinintsoa1234@gmail.com" 
